63ea0eb2c89843104882ff28f98e9443481616a1
jasmoon <fourdusk4@gmail.com>
2020-03-21 21:25:26 +0800

    Added http parameter pollution check for GET request

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 Tested using Python 3.6.4.
 
+### Requirements
+Below are the python packages required:
+* `werkzeug`
+
+Use `pip` to install the required packages before running the program.
+
+## Instructions to use
 To start the WAF, use `python main.py <port-to-listen> <web-app-port>`.
 
 `waf_handler.py` handles the functionality of the WAF. The `do_GET`
-and `do_POST` methods of the handler handles the respective request.
+and `do_POST` methods of the handler handles the respective requests.
 Thus, to add functionalities, edit the respective methods.
--- waf_handler.py
+++ waf_handler.py
@@ -1,16 +1,38 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
+from werkzeug import urls
 
 
 class WAFHandler(BaseHTTPRequestHandler):
 
     # Handler for the GET requests
     def do_GET(self):
+        def check_para_pollution(url_path):
+            qns_idx = url_path.find('?')
+            if qns_idx > -1:
+                url_path = url_path[qns_idx+1:]
+                parameters = urls.url_decode(url_path).lists()
+                for k, v in parameters:
+                    if len(v) > 1:
+                        return True
+            return False
+
+        path = self.path
         req_header = self.headers
-        print(req_header)
+        msg = ""
+
+        if check_para_pollution(path):
+            msg += u"HTTP parameter pollution detected"
+
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
+
+        if len(msg) == 0:
+            msg = u"Hello World !"
+
         # Send the html message
-        msg = u"Hello World !"
         self.wfile.write(msg.encode("utf8"))
         return
+
+    def do_POST(self):
+        req_header = self.headers
